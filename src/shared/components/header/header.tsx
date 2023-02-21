397e2fbdca68e5ebe8a5f6fd532c3745bce75840
Roy Mosby <ego.madking@gmail.com>
2023-02-21 08:05:01 -0500

    add style to div

--- src/shared/components/header/header.tsx
+++ src/shared/components/header/header.tsx
@@ -1,7 +1,11 @@
-export function Header(){
-  return(
-    <div>
+import { Nav } from '../index';
+import styles from './header.module.scss';
+
+export function Header() {
+  return (
+    <div className={styles.navBar}>
       <h2>Header</h2>
+      <Nav></Nav>
     </div>
-  )
+  );
 }
