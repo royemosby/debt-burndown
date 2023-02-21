dc618ef79ce4bc0c441732f089dd8138c600e69d
Roy Mosby <ego.madking@gmail.com>
2023-02-21 08:34:47 -0500

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { router } from './shared/routes/routes';
-import { Header } from './shared/components';
 
 function App() {
   return (
