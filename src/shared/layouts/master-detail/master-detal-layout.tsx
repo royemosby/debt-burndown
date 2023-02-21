f9285c7b19ead2c6908042c5b40f88ee2107deb0
Roy Mosby <ego.madking@gmail.com>
2023-02-21 08:25:45 -0500

    componentize sub-elements

--- src/shared/layouts/master-detail/master-detal-layout.tsx
+++ src/shared/layouts/master-detail/master-detal-layout.tsx
@@ -1,20 +1,16 @@
 import { Header, DebtCard } from '@/shared/components';
 import { Debt } from '@/features/home/home';
 import styles from './master-detail.module.scss';
+import { Details } from './details/details';
+import { MasterList } from './master-list/master-list';
 
 export function MasterDetailLayout({ debts, children }: { debts: Debt[]; children?: any }) {
   return (
     <>
       <Header />
       <div className={styles.masterDetails}>
-        <ul className="master">
-          {debts.map(debt => (
-            <DebtCard key={debt.name} debt={debt} />
-          ))}
-        </ul>
-        <div className="detail">
-          <h2>Detailed view</h2>
-        </div>
+        <MasterList debts={debts} />
+        <Details />
       </div>
     </>
   );
